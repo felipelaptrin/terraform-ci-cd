2cc41902e12f9a46aae18a9585676b15f6802523
Felipe Trindade <52676348+felipelaptrin@users.noreply.github.com>
2025-02-17 16:45:18 -0300

    test plan (#5)
    * test plan
    
    * fix backend issue
    
    * remove bucket

--- src/main.tf
+++ src/main.tf
@@ -1,14 +1,3 @@
-resource "aws_kms_key" "s3" {
-  description = "KMS key user to encrypt buckets"
-}
-
-module "bucket-test" {
-  source = "../modules/bucket"
-
-  bucket_name = "test"
-  kms_key_arn = aws_kms_key.s3.arn
-}
-
 resource "aws_sqs_queue" "terraform_queue" {
   name                    = "test"
   sqs_managed_sse_enabled = true
