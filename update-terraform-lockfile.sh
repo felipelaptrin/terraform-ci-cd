38c0455234b90867f8a213ecfa79a0897123a48b
felipelaptrin <paotrindadepao@gmail.com>
2025-02-17 13:48:28 -0300

    add lock

--- update-terraform-lockfile.sh
+++ update-terraform-lockfile.sh
@@ -3,11 +3,10 @@ set -e
 
 echo "Updating Terraform lock file for multiple platforms..."
 
+cd src/
+
 terraform providers lock \
--platform=windows_amd64 \
--platform=darwin_amd64 \
 -platform=linux_amd64 \
--platform=darwin_arm64 \
--platform=linux_arm64
+-platform=darwin_arm64
 
 echo "Terraform lock file updated!"
